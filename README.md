d52a40d0437e0fcda6e4e8d7ae40e9176a28b9c9
Chase Seibert <chase.seibert+github@gmail.com>
2015-04-17 10:30:39 -0700

    added views.py

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mini-practicum
-mini-practicum
+
+This is the basis of an interview question where candidates are asked to make constructive criticism of a pull request.
+
+*Note: the code is intentionally horrible. Avert your eyes!*
---
+++ views.py
@@ -0,0 +1,12 @@
+from framework import require_post
+from framework import reverse
+from framework import HttpResponseRedirect
+from myapp.models import User
+
+
+@require_post
+def create_user(request):
+    if request.method == 'POST':
+        username, password = request.POST.get('username'), request.POST.get('password')
+        user = User.create(username, password)
+        return HttpResponseRedirect(reverse('user', args=(user.id, )))
